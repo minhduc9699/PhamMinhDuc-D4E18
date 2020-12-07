eb21df7c365da170ddd50cd287425872fbaec2f1
Phạm Đức <minhduc.096.99@gmail.com>
2020-12-07 21:19:49 +0700

    session 4

--- session_4/shop.py
+++ session_4/shop.py
@@ -1,20 +1,22 @@
 shop = ['T-shirt', 'Hoodie']
+action = input('welcome to our shop, what do you want? (C R U D): ')
 
-# C
-new_item = input('Enter new stuff')
-shop.append(new_item)
-print(shop)
-
-# R
-for i in range(len(shop)):
-    print(f'{i+1}.{shop[i]}')
-
-# U
-update_position = int(input('Enter update position: '))
-shop[update_position] = input('Enter new item: ')
-print(shop)
-
-# D
-delete_position = int(input('Enter delete position: '))
-shop.pop(delete_position)
-print(shop)
+if action == 'C':
+    # C
+    new_item = input('Enter new stuff')
+    shop.append(new_item)
+    print(shop)
+elif action == 'R':
+    # R
+    for i in range(len(shop)):
+        print(f'{i+1}.{shop[i]}')
+elif action == 'U':
+    # U
+    update_position = int(input('Enter update position: '))
+    shop[update_position] = input('Enter new item: ')
+    print(shop)
+elif action == 'D':
+    # D
+    delete_position = int(input('Enter delete position: '))
+    shop.pop(delete_position)
+    print(shop)
