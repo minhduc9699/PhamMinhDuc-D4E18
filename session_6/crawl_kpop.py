85343af70b22d6a0e3926c605502f2d713c9512d
Phạm Đức <minhduc.096.99@gmail.com>
2020-12-20 10:48:53 +0700

    .

--- session_6/crawl_kpop.py
+++ session_6/crawl_kpop.py
@@ -3,5 +3,9 @@ from bs4 import BeautifulSoup
 import pyexcel
 
 response = get('https://dbkpop.com/db/all-k-pop-idols')
-file = open('kpop.html', 'w', encoding='utf-8')
-file.write(response.text)
+# file = open('kpop.html', 'w', encoding='utf-8')
+content_html = BeautifulSoup(response.text)
+# file.write(content_html.prettify())
+
+table_html = content_html('table', {'id': 'table_1'})
+print(table_html)
