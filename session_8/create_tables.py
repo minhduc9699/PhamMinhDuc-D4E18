3019c365beaeda4ce6894d085224447244dc227f
Phạm Đức <minhduc.096.99@gmail.com>
2021-01-04 21:49:59 +0700

    session_9

---
+++ session_8/create_tables.py
@@ -0,0 +1,21 @@
+from connect import init_connection
+
+connection = init_connection()
+
+board = connection[2]
+
+# board.execute('''
+#     CREATE TABLE `vgsales`.`region` (
+#         id INT(11) PRIMARY KEY,
+#         name VARCHAR(255)
+#     )
+# ''')
+
+board.execute('''
+    CREATE TABLE `vgsales`.`video_game_sales`(
+        video_game_id INT(11) REFERENCES `vgsales`.`video_game`(id),
+        region_id INT(11) REFERENCES `vgsales`.`region`(id),
+        sales DECIMAL(19,2)
+    )
+''')
+
